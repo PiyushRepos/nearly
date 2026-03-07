e3a7d53937da075ed90371f81125bb0635c2fec0
Piyush Kumar <piyush2004.dev@gmail.com>
2026-03-08 01:14:49 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1>Welcome to my React App!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
